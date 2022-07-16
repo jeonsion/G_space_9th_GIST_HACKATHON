cb84d681ce29f4610eac8d5d62428bcb52180459
Midnight Coding <57317636+jeonsion@users.noreply.github.com>
2022-07-17 00:08:04 +0900

    Update IR_post_server.py

--- IR_post_server.py
+++ IR_post_server.py
@@ -1,5 +1,5 @@
 #모터가 구동되는 상태에서 IR이 Phone/count 값 만큼 약을 카운트했을 때 서버에 0을 올리는 코드
-
+#서버에 0을 올림으로 써 추후게 모터를 중지시킬 
 #importing the library of RPi.GPIO
 #importing the library of time
 from http import server
@@ -19,27 +19,14 @@ while True:
     
     #서버에서 count 값을 Get
     url_count_get = 'http://203.253.128.177:7579/Mobius/phone/count/la'
-    #IR에서 서버에 data 값을 Post
-    url_count_post ='http://203.253.128.177:7579/Mobius/phone/data/la'
-
-    headers_response_post = {
-        'Accept': 'application/json',
-        'X-M2M-RI': '12345',
-        'X-M2M-Origin': 'SluN3OkDey-',
-        'Content-Type': 'application/vnd.onem2m-res+json; ty=4'
-    }
-
+ 
     headers_count_get = {
         'Accept': 'application/json',
         'X-M2M-RI': '12345',
         'X-M2M-Origin': 'SluN3OkDey-',
         'Content-Type': 'application/vnd.onem2m-res+json; ty=4'
     } 
-    data = {
-        "m2m:cin": {
-            "con" : "response_success : 0"
-        }
-    }
+   
     
     r_count_get = requests.get(url_count_get, headers = headers_count_get)
     r_count_get.raise_for_status()
@@ -57,8 +44,25 @@ while True:
         count+=1
         if(server_count == count ):
             print("잘 들어왔습니다, 서버카운트 값 : {}, 알약이 나온 개수 : {}".format(server_count, count))
-            #r_response_post = requests.post(url_count_post, headers = headers_response_post, json = data )
-            #r_response_post.raise_for_status()
+            
+            #*****************Post cin****************************
+            url = "http://203.253.128.177:7579/Mobius/phone/data"
+
+            headers = {
+            'Accept': 'application/json',
+            'X-M2M-RI': '12345',
+            'X-M2M-Origin': 'SluN3OkDey-',
+            'Content-Type': 'application/vnd.onem2m-res+json; ty=4'
+            }
+
+            data = {
+                "m2m:cin": {
+                    "con" : "0"
+                }
+            }
+
+            r = requests.post(url, headers = headers, json = data)
+            #*****************Post cin******************************
             break
         print("count : {}, Object detected".format(count))
         while GPIO.input(sensor):  
